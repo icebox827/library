f3a811606a8fefb1f44386d6557c66dc7cf23a3a
Denis Lafontant <icebox827@gmail.com>
2021-03-16 21:40:36 -0400

    Add new assets

--- assets/Script/main.js
+++ assets/Script/main.js
@@ -1,40 +1,62 @@
 let myLibrary=[];
 
-function Book(title, author, pages, read = false){
-  this.author=author;
+function Book(title, author, pages, read = false) {
+  this.author = author;
   this.title = title;
   this.pages = pages;
   this.read = read;
 }
 
-function addBookToLibrary(myLibrary){
-  let form = document.getElementById('input_book');
-   form.addEventListener('submit', (event) => {
-  let bookName = form.elements[0];
-  let bookTitle = form.elements[1];
-  let bookPages = form.elements[2];
-  let bookRead = form.elements[3];
+function addBookToLibrary(book) {
+//   let form = document.getElementById('input_book');
+//    form.addEventListener('submit', (event) => {
+//   let bookName = form.elements[0];
+//   let bookTitle = form.elements[1];
+//   let bookPages = form.elements[2];
+//   let bookRead = form.elements[3];
 
- })
+//  })
 
-  if (bookRead !== 'No'){
-    read = true
-  } 
+//   if (bookRead !== 'No'){
+//     read = true
+//   } 
 
-  book = new Book(bookName, bookTitle, bookPages, read)
-  myLibrary.push(book)
-  return myLibrary
+  const newbook = new Book(author, title, pages, read)
+  myLibrary.push(newbook)
 }
 
-function displayBook(myLibrary) {
+function displayBook(book) {
+  document.getElementById("bookShelves").innerHTML = "";
+
   for (let i = 0; i < myLibrary.length; i++) {
-    // console.log(myLibrary[i])  
-    card.id = bookAuthor
-    card.text = bookTitle
-    card.body = bookPages
-    card.text = bookReads
+    let cardElement = document.createElement("div");
+    let imageContainer = document.createElement("div");
+    let infoContainer = document.createElement("div");
+    let image = document.createElement("img");
+    let author = document.createElement("h3");
+		let title = document.createElement("h4");
+		let pages = document.createElement("h5");
+    let read = document.createElement("h5");
+    let btnRead = document.createElement("button");
+    let btnDelete = document.createElement("button");
+
+		cardElement.className = "card";
+    imageContainer.className = "img-card";
+    infoContainer.className = "info-card";
+    image.className = "image";
+    author.className = "author";
+		title.className = "title";
+		pages.className = "pages";
+    read.className = "read";
+    btnRead.className = "btn"
+    btnDelete.className = "btn"
+
+    image.src = "./assets/img/library_of_alexandria.jpeg"
+    image.setAttribute("alt", "Image from assets")
+		 
   }
 }
 
-library = addBookToLibrary(myLibrary)
-show = displayBook(myLibrary)
+function changeStatus(book){
+  myLibrary[book].read !== false ? myLibrary[book].read == true : myLibrary[book].read == false
+}
