2c958a26f498e90da85bab5c64fa7d1152945df9
Denis Lafontant <icebox827@gmail.com>
2021-03-17 11:56:34 -0400

    Add JS element

--- assets/Script/main.js
+++ assets/Script/main.js
@@ -1,3 +1,5 @@
+let myLibrary = [{"author":"Gustavo","title":"My book","pages":"994","read":false}, 
+{"author":"Denis","title":"His book","pages":"123","read":false}]
 
 function Book(title, author, pages, read = false) {
   this.author = author;
@@ -21,30 +23,36 @@ function createNewBook(){
   const book = new Book(bookAuthor, bookTitle, bookPages)
 
   myLibrary.push(book)
-  console.log(JSON.stringify(myLibrary))
-  text = document.createElement("p")
-  text.setAttribute("id", "storage")
-  text.innerHTML = JSON.stringify(myLibrary)
-  document.body.appendChild(text)
+  // console.log(JSON.stringify(myLibrary))
+  // text = document.createElement("p")
+  // text.setAttribute("id", "storage")
+  // text.innerHTML = JSON.stringify(myLibrary)
+  // document.body.appendChild(text)
 }
 
+function displayBook(myLibrary) {
+  document.getElementById("card-container").innerHTML = "";
 
-function displayBook(book) {
-  for(i = 0; i < myLibrary.length; i++) {
-    let author = document.getElementById("author").value;
-    let title = document.getElementById("title").value;
-    let pages = document.getElementById("pages").value;
-    let read = document.getElementById("read").value;
+	for(let i = 0; i < myLibrary.length; i++) {
+    let card = document.createElement("div");
+    let author = document.createElement("div");
+    let title = document.createElement("div");
+    let pages = document.createElement("div");
+    card.className = "card"
+		author.className = "author";
+    title.className = "title";
+    pages.className = "pages";
 
-    author.innerHTML = author;
-    title.innerHTML = title;
-    pages.innerHTML = pages;
-    read.innerHTML = read;
-    
+    card.innerHTML = myLibrary[i].Value;
+		author.innerHTML = myLibrary[i].Value;
+    title.innerHTML = myLibrary[i].Value;
+    pages.innerHTML = myLibrary[i].Value;
+		card.appendChild(author);
+		card.appendChild(title);
+    card.appendChild(pages);
 
-    image.src = "./assets/img/library_of_alexandria.jpeg"
-    image.setAttribute("alt", "Image from assets")
-  }
+		document.getElementById("myLibrary").appendChild(card);
+	}
 }
 
 function changeStatus(book){
