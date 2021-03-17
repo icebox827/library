f837bd2406196761daf2f1cfa1630e7eef7d3cd0
tavo <gasb_150@hotmail.com>
2021-03-17 10:56:50 -0500

    Show info

--- assets/Script/main.js
+++ assets/Script/main.js
@@ -10,40 +10,47 @@ function Book(title, author, pages, read = false) {
 
 function createNewBook(){
   
-  let myLibrary = [{"author":"Gustavo","title":"My book","pages":"994","read":false}, 
-  {"author":"Denis","title":"His book","pages":"123","read":false}]
-  
+  let myLibrary=[]
+  const book1 = new Book("My book","Gustavo", 994);
+  const book2 = new Book("His book","Denis", 123);
+     myLibrary.push(book1);
+     myLibrary.push(book2);
   let form = document.getElementById('input_book');
-  let bookAuthor = form.elements[0].value;
-  let bookTitle = form.elements[1].value;
+  let bookAuthor = form.elements[1].value;
+  let bookTitle = form.elements[0].value;
   let bookPages = form.elements[2].value;
 
-  const book = new Book(bookAuthor, bookTitle, bookPages)
+  const book3 = new Book(bookAuthor, bookTitle, bookPages)
 
-  myLibrary.push(book)
+  myLibrary.push(book3)
   console.log(JSON.stringify(myLibrary))
   text = document.createElement("p")
-  text.setAttribute("id", "storage")
-  text.innerHTML = JSON.stringify(myLibrary)
+  // text.setAttribute("id", "storage")
+  // text.innerHTML = JSON.stringify(myLibrary)
   document.body.appendChild(text)
+  return myLibrary
 }
 
 
-function displayBook(book) {
+function displayBook() {
+  let myLibrary = createNewBook()
+  console.log(myLibrary)
   for(i = 0; i < myLibrary.length; i++) {
-    let author = document.getElementById("author").value;
-    let title = document.getElementById("title").value;
-    let pages = document.getElementById("pages").value;
-    let read = document.getElementById("read").value;
+    
+    const author = document.getElementById("author");
+    const title = document.getElementById("title");
+    const pages = document.getElementById("pages");
+    const read = document.getElementById("read");
 
-    author.innerHTML = author;
-    title.innerHTML = title;
-    pages.innerHTML = pages;
-    read.innerHTML = read;
+    
+    author.innerHTML = myLibrary[i].author;
+    title.innerHTML = myLibrary[i].title;
+    pages.innerHTML = myLibrary[i].pages;
+    read.innerHTML = myLibrary[i].read;
     
 
-    image.src = "./assets/img/library_of_alexandria.jpeg"
-    image.setAttribute("alt", "Image from assets")
+    // image.src = "./assets/img/library_of_alexandria.jpeg"
+    // image.setAttribute("alt", "Image from assets")
   }
 }
 
