61901e8158fb9c232da7e21a0c8d40d28883723a
Denis Lafontant <icebox827@gmail.com>
2021-03-23 15:42:06 -0400

    Add window method

--- assets/Script/main.js
+++ assets/Script/main.js
@@ -1,7 +1,7 @@
 const myLibrary = [];
 const btn = document.getElementById('submit_btn');
 
-function storageMyLibrary {
+function storageMyLibrary() {
   localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
 }
 
@@ -15,7 +15,7 @@ class Book {
 }
 
 const addBook = () => {
-  
+  let myLibrary = JSON.parse(localStorage.getItem('myLibrary'));
   const form = document.getElementById('form').value
   const author = document.getElementById('author').value;
   const title = document.getElementById('title').value;
@@ -24,64 +24,68 @@ const addBook = () => {
 
   const book = new Book(author, title, pages, read);
   myLibrary.push(book);
+  storageMyLibrary(myLibrary);
+  reloadPage();
   
   btn.addEventListener('click', () => {
     addBook();
   });
-
-  displayBook(book);
 }
 
-const displayBook = (book) => {
-  for(i = 0; i < myLibrary.length; i += 1){
-    if (myLibrary[i].author !== '') {
-      const card = document.createElement('card');
-      const author = document.createElement('div');
-      const title = document.createElement('div');
-      const pages = document.createElement('div');
-      const read = document.createElement('button');
-      const removeButton = document.createElement('button');
-
-      read.onclick = function toggleContent() {
-        const id = this.id.slice(10);
-        const readStatus = myLibrary[id].read;
-        if (readStatus === 'Not read') {
-          myLibrary[id].read = 'read!';
-        } else {
-          myLibrary[id].read = 'Not read';
-        }
-        storageMyLibrary(myLibrary);
-        reloadPage();
-      };
-
-      removeButton.onclick = function toRemove() {
-        remove(this.id);
-        reloadPage();
-      };
-
-      card.className = 'card';
-      author.className = 'card-author';
-      title.className = 'card-title';
-      pages.className = 'card-pages';
-      read.className = 'card-read';
-      read.id = `readButton${i}`;
-      removeButton.id = `remove${i}`;
-
-      card.setAttribute('id', `card${i} `);
-
-      document.getElementById('card-container').appendChild(card);
-      document.getElementById(`card${i} `).appendChild(author);
-      document.getElementById(`card${i} `).appendChild(title);
-      document.getElementById(`card${i} `).appendChild(pages);
-      document.getElementById(`card${i} `).appendChild(read);
-      document.getElementById(`card${i} `).appendChild(removeButton);
-
-      author.innerHTML = myLibrary[i].author;
-      title.innerHTML = myLibrary[i].title;
-      pages.innerHTML = myLibrary[i].pages;
-      read.innerHTML = myLibrary[i].read;
-      removeButton.innerHTML = 'Remove';
+window.addEventListener('load', () => {
+  const displayBook = (book) => {
+    let myLibrary = JSON.parse(localStorage.getItem('myLibrary'));
+    for(i = 0; i < myLibrary.length; i += 1){
+      if (myLibrary[i].author !== '') {
+        const card = document.createElement('card');
+        const author = document.createElement('div');
+        const title = document.createElement('div');
+        const pages = document.createElement('div');
+        const read = document.createElement('button');
+        const removeButton = document.createElement('button');
+  
+        read.onclick = function toggleContent() {
+          const id = this.id.slice(10);
+          const readStatus = myLibrary[id].read;
+          if (readStatus === 'Not read') {
+            myLibrary[id].read = 'read!';
+          } else {
+            myLibrary[id].read = 'Not read';
+          }
+          storageMyLibrary(myLibrary);
+          reloadPage();
+        };
+  
+        removeButton.onclick = function toRemove() {
+          remove(this.id);
+          reloadPage();
+        };
+  
+        card.className = 'card';
+        author.className = 'card-author';
+        title.className = 'card-title';
+        pages.className = 'card-pages';
+        read.className = 'card-read';
+        read.id = `readButton${i}`;
+        removeButton.id = `remove${i}`;
+  
+        card.setAttribute('id', `card${i} `);
+  
+        document.getElementById('card-container').appendChild(card);
+        document.getElementById(`card${i} `).appendChild(author);
+        document.getElementById(`card${i} `).appendChild(title);
+        document.getElementById(`card${i} `).appendChild(pages);
+        document.getElementById(`card${i} `).appendChild(read);
+        document.getElementById(`card${i} `).appendChild(removeButton);
+  
+        author.innerHTML = myLibrary[i].author;
+        title.innerHTML = myLibrary[i].title;
+        pages.innerHTML = myLibrary[i].pages;
+        read.innerHTML = myLibrary[i].read;
+        removeButton.innerHTML = 'Remove';
+      }
     }
   }
-}
+});
+
 
